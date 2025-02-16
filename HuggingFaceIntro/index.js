1d00898a107113c950678e59c4ff6c29f5128d44
Plamen Petrov <biznetmen@gmail.com>
2025-02-16 14:00:58 +0200

    translation model

--- HuggingFaceIntro/index.js
+++ HuggingFaceIntro/index.js
@@ -1,6 +1,10 @@
 /** @format */
 
 import { HfInference } from '@huggingface/inference';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Create your Hugging Face Token: https://huggingface.co/settings/tokens
 // Set your Hugging Face Token: https://scrimba.com/dashboard#env
@@ -8,3 +12,21 @@ import { HfInference } from '@huggingface/inference';
 const hf = new HfInference(process.env.HF_TOKEN);
 
 // Hugging Face Inference API docs: https://huggingface.co/docs/huggingface.js/inference/README
+
+// Text translation
+const textToTranslate = "It's an exciting time to be an AI engineer";
+
+// https://huggingface.co/facebook/mbart-large-50-many-to-many-mmt#languages-covered
+
+const textTranslationResponse = await hf.translation({
+	model: 'facebook/mbart-large-50-many-to-many-mmt',
+	inputs: textToTranslate,
+	parameters: {
+		src_lang: 'en_XX',
+		tgt_lang: 'ur_PK',
+	},
+});
+
+const translation = textTranslationResponse.translation_text;
+console.log('\ntranslation:\n');
+console.log(translation);
