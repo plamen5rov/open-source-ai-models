6aae1fe08255b43906b8173daf8dad55c257b036
Plamen Petrov <biznetmen@gmail.com>
2025-02-16 12:40:52 +0200

    initial setup

---
+++ HuggingFaceIntro/index.js
@@ -0,0 +1,8 @@
+import { HfInference } from '@huggingface/inference'
+
+// Create your Hugging Face Token: https://huggingface.co/settings/tokens
+// Set your Hugging Face Token: https://scrimba.com/dashboard#env
+// Learn more: https://scrimba.com/links/env-variables
+const hf = new HfInference(process.env.HF_TOKEN)
+
+// Hugging Face Inference API docs: https://huggingface.co/docs/huggingface.js/inference/README
