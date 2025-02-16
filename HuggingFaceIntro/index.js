4969434cbfe0932b1f09c2e56746ff9f6ca4c58d
Plamen Petrov <biznetmen@gmail.com>
2025-02-16 13:31:54 +0200

    npm install @huggingface/inference

--- HuggingFaceIntro/index.js
+++ HuggingFaceIntro/index.js
@@ -1,8 +1,10 @@
-import { HfInference } from '@huggingface/inference'
+/** @format */
+
+import { HfInference } from '@huggingface/inference';
 
 // Create your Hugging Face Token: https://huggingface.co/settings/tokens
 // Set your Hugging Face Token: https://scrimba.com/dashboard#env
 // Learn more: https://scrimba.com/links/env-variables
-const hf = new HfInference(process.env.HF_TOKEN)
+const hf = new HfInference(process.env.HF_TOKEN);
 
 // Hugging Face Inference API docs: https://huggingface.co/docs/huggingface.js/inference/README
